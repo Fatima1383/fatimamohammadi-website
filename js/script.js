bae669675412ed5e75f846c872199e8bec856da3
Fatima1383 <fofi.moh2050@gmail.com>
2025-11-18 03:02:04 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-window.onscroll = () => {
+Let menuIcon = document.querySelector('#menu-icon');
+Let navbar = document.querySelector('navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+}
+
+window.onscroll = ( ) => {
+    
+}
 
 let header = document.querySelector('header');
 header.classList.toggle('sticky', window.scrollY > 100);
 }
+
+
+
+
 
 
 
